3f9d1392cfcd9ae42a7955c771bfb661f2372bac
Mihai Lascu <mita@192-168-0-115.rdsnet.ro>
2018-12-16 13:16:21 +0200

    added Portfolio and Cryptocurrency models

--- CryptoWatcher/mysite/crypto/models.py
+++ CryptoWatcher/mysite/crypto/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Portfolio(models.Model):
+    password = models.CharField(max_length=300)
+
+class Cryptocurrency(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    coin_symbol = models.CharField(max_length=10)
+    coin_amount = models.FloatField(default=0.0)
+    coin_investment = models.FloatField(default=0.0)
--- CryptoWatcher/mysite/mysite/settings.py
+++ CryptoWatcher/mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crypto',
+    'crypto.apps.CryptoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
