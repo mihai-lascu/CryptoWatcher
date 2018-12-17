07c68c108d3feb8db52e3c9d8d784dd7a51fe056
Mihai Lascu <mita@192-168-0-115.rdsnet.ro>
2018-12-17 18:41:29 +0200

    worked out the password system for the portfolio models

--- CryptoWatcher/mysite/crypto/views.py
+++ CryptoWatcher/mysite/crypto/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from passlib.hash import pbkdf2_sha256
+
+from .models import Portfolio, Cryptocurrency
 
 import json
 import requests
@@ -7,7 +12,7 @@ import requests
 
 def home(request):
     #Grab the crypto_list
-    crypto_list_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,XLM,EOS,LTC,TRX,ADA,XMR,MIOTA,XEM,DASH,ETC&tsyms=USD")
+    crypto_list_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,XRP,ETH,EOS,XLM,LTC,TRX,ADA,XMR,IOT,DASH,XEM,ETC&tsyms=USD")
     crypto_list = json.loads(crypto_list_request.content)
 
     #Grab the news
@@ -24,17 +29,36 @@ def prices(request):
 def portfolio(request):
     if request.method == 'POST':
         portfolio_id = request.POST['portfolio_id']
-        return render(request, 'crypto/portfolio.html', {'portfolio_id': portfolio_id})
+        try:
+            portfolio = Portfolio.objects.get(pk=portfolio_id)
+        except (KeyError, Portfolio.DoesNotExist):
+            return render(request, 'crypto/portfolio.html', {'notfound':'Wrong portfolio ID. Try again.'})
+        else:
+            return HttpResponseRedirect(reverse('crypto:portfolio_edit', args=(portfolio.id,)))
     else:
-        notfound = "Portfolio feature coming soon"
-        return render(request, 'crypto/portfolio.html', {'notfound': notfound })
+        return render(request, 'crypto/portfolio.html', {})
 
 def portfolioCreate(request):
-    portfolio_id = 1234
-    return render(request, 'crypto/portfolio_create.html', {'portfolio_id': portfolio_id })
+    new_portfolio = Portfolio(password='')
+    new_portfolio.save()
+    return render(request, 'crypto/portfolio_create.html', {'new_portfolio': new_portfolio })
 
 def portfolioEdit(request, portfolio_id):
-    portfolio_password = "fake"
     if request.method == 'POST':
-        portfolio_password = request.POST['portfolio_password']
-    return render(request, 'crypto/portfolio_edit.html', {'portfolio_id': portfolio_id, 'portfolio_password': portfolio_password})
+        if 'new_password' in request.POST.keys():
+            portfolio_password = request.POST['new_password']
+            portfolio = get_object_or_404(Portfolio, pk=portfolio_id)
+            portfolio.password = pbkdf2_sha256.hash(portfolio_password)
+            portfolio.save()
+            return render(request, 'crypto/portfolio_edit.html', {'portfolio_id': portfolio_id, 'password_correct': True})
+            #return HttpResponseRedirect(reverse('crypto:portfolio_edit', args=(portfolio.id,)))
+        elif 'portfolio_password' in request.POST.keys():
+            portfolio_password = request.POST['portfolio_password']
+            portfolio = get_object_or_404(Portfolio, pk=portfolio_id)
+            if pbkdf2_sha256.verify(portfolio_password, portfolio.password):
+                return render(request, 'crypto/portfolio_edit.html', {'portfolio_id': portfolio_id, 'password_correct': True})
+            else:
+                return render(request, 'crypto/portfolio_edit.html', {'portfolio_id': portfolio_id, 'password_incorrect': True})
+    else:
+        portfolio = get_object_or_404(Portfolio, pk=portfolio_id)
+        return render(request, 'crypto/portfolio_edit.html', {'portfolio_id': portfolio_id, 'type_password': 'You need to type your password'})
