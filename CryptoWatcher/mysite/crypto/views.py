689a814ae1ab825358c717a0e1eb05e5ca3780ac
Mihai Lascu <mita@192-168-0-102.rdsnet.ro>
2018-12-28 17:31:45 +0200

    removing import HttpResponseReditect and using import redirect

--- CryptoWatcher/mysite/crypto/views.py
+++ CryptoWatcher/mysite/crypto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
